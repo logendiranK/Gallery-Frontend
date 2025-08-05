6acc6dcca4cb11a09e6e73616c652cc0e469be02
logendiranK <logendiran2005@gmail.com>
2025-08-05 19:05:39 +0530

    appcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {  BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import ArtworkGallery from './Artwork';
+import Artwork from './Artwork.jsx';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Home from './Home';
@@ -15,7 +15,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/artwork" element={<ArtworkGallery />} />
+        <Route path="/artwork" element={<Artwork />} />
         <Route path="/about" element={<About/>} />
         <Route path="/afefwegwerwegwerffgw4erwfgwef" element={<Admin />} />
         <Route path="/check" element={<Check />} />
